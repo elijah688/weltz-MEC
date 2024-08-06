841bfdf34b215bd4813c7501eda6f52716557d2e
Elijah_Iliev <elijah@Elijahs-MacBook-Pro-2.local>
2024-08-06 18:49:00 +0100

    scale

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"math"
 	"sort"
@@ -9,60 +10,89 @@ import (
 )
 
 func main() {
-	const width = 1024
-	const height = 1024
+	const width = 1000
+	const height = 1000
 
 	dc := gg.NewContext(width, height)
 	dc.SetRGB(1, 1, 1)
 	dc.Clear()
 
-	dc.Translate(width/2, height/2) // Make (0,0) the center of the canvas
-
-	dc.SetRGB(0, 0, 0)
-
 	points := []gg.Point{
-		{X: 120, Y: 450},
-		{X: 120, Y: 400},
-		{X: 150, Y: 400},
-		{X: 180, Y: 450},
-		{X: 170, Y: 450},
-		{X: 200, Y: 400},
-		{X: 230, Y: 450},
-		{X: 260, Y: 400},
-		{X: 260, Y: 350},
-		{X: 230, Y: 300},
-		{X: 150, Y: 350},
-		{X: 666, Y: 432},
-		{X: 153, Y: 225},
+		{X: -3.705433, Y: 40.417323},
+		{X: -3.702666, Y: 40.419283},
+		{X: -3.705621, Y: 40.418924},
+		{X: -3.704087, Y: 40.416533},
+		{X: -3.701512, Y: 40.417971},
+		{X: -3.701319, Y: 40.419196},
+		{X: -3.701212, Y: 40.420045},
+		{X: -3.703272, Y: 40.418804},
+		{X: -3.700118, Y: 40.417382},
+		{X: -3.700955, Y: 40.414458},
+		{X: -3.700633, Y: 40.412351},
+		{X: -3.705417, Y: 40.412939},
+		{X: -3.703658, Y: 40.414197},
+		{X: -3.713311, Y: 40.413625},
+		{X: -3.707605, Y: 40.416223},
+		{X: -3.711896, Y: 40.416549},
+		{X: -3.712067, Y: 40.42132},
+		{X: -3.707562, Y: 40.421336},
+		{X: -3.713268, Y: 40.41802},
+		{X: -3.717344, Y: 40.416043},
 	}
+
+	// Get bounding box
+	minX, minY, maxX, maxY := points[0].X, points[0].Y, points[0].X, points[0].Y
+	for _, p := range points {
+		if p.X < minX {
+			minX = p.X
+		}
+		if p.Y < minY {
+			minY = p.Y
+		}
+		if p.X > maxX {
+			maxX = p.X
+		}
+		if p.Y > maxY {
+			maxY = p.Y
+		}
+	}
+
+	scaleX := float64(width) / (maxX - minX)
+	scaleY := float64(height) / (maxY - minY)
+	scale := math.Min(scaleX, scaleY)
+
+	centerX := (maxX + minX) / 2.0
+	centerY := (maxY + minY) / 2.0
+
+	offsetX := (width / 2.0) - (centerX * scale)
+	offsetY := (height / 2.0) - (centerY * scale)
 
 	dc.SetRGB(1, 0, 0)
 	for _, point := range points {
-		dc.DrawPoint(point.X, point.Y, 3)
+		scaledX := point.X*scale + offsetX
+		scaledY := point.Y*scale + offsetY
+		dc.DrawPoint(scaledX, scaledY, 3)
 		dc.Fill()
 	}
 
 	dc.SetRGB(0, 1, 0)
 	dc.NewSubPath()
-	for _, p := range sortPoints(points) {
-		dc.LineTo(p.X, p.Y)
+	for _, p := range points {
+		scaledX := p.X*scale + offsetX
+		scaledY := p.Y*scale + offsetY
+		dc.LineTo(scaledX, scaledY)
 	}
 	dc.ClosePath()
 	dc.Stroke()
 
 	c1 := FindMinEnclosingCircle(points)
 
-	circles := []struct {
-		X, Y, Radius float64
-	}{
-		{X: c1.C.X, Y: c1.C.Y, Radius: c1.R},
-	}
-
 	dc.SetRGB(0, 0, 1)
-	for _, circle := range circles {
-		dc.DrawCircle(circle.X, circle.Y, circle.Radius)
-		dc.Stroke()
-	}
+	scaledCX := c1.C.X*scale + offsetX
+	scaledCY := c1.C.Y*scale + offsetY
+	scaledRadius := c1.R * scale
+	dc.DrawCircle(scaledCX, scaledCY, scaledRadius)
+	dc.Stroke()
 
 	err := dc.SavePNG("output.png")
 	if err != nil {
@@ -70,6 +100,7 @@ func main() {
 	}
 
 	log.Println("Image successfully saved as output.png")
+	fmt.Println(c1)
 }
 
 func centroid(points []gg.Point) gg.Point {
